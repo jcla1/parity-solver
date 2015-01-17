5759822d43e52f0587fd1de5ecd634b90252d770
Joseph Adams <whitegolem@gmail.com>
2015-01-17 22:47:36 +0100

    Update parity-solver.hs

--- parity-solver.hs
+++ parity-solver.hs
@@ -81,4 +81,6 @@ findCompletionPath gs =
 main :: IO ()
 main = putStrLn $ findCompletionPath gs
   where
+    -- This GameState represents the 4th level of Parity
+    -- http://www.abefehr.com/parity/
     gs = GameState (0,0) (Board (3,3) [3,4,3,1,3,2,1,1,2])
