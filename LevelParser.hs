820053dddb89ef39948d6883cf00a12152d1cb66
Joseph Adams <whitegolem@gmail.com>
2015-01-18 15:35:00 +0100

    Whew! Add parser for the levels in JSON format.

---
+++ LevelParser.hs
@@ -0,0 +1,47 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+-- Adapted from:
+-- http://blog.raynes.me/blog/2012/11/27/easy-json-parsing-in-haskell-with-aeson/
+module LevelParser where
+import Control.Applicative ((<$>), (<*>))
+import Control.Monad (liftM)
+import qualified Data.ByteString.Lazy.Char8 as BS
+import qualified Data.HashMap.Strict as HM
+import Data.Maybe
+import Data.Scientific (coefficient)
+import qualified Data.Text as T
+import qualified Data.Vector as V
+import Debug.Trace
+
+import Data.Aeson ((.:), (.:?), decode, FromJSON(..), Value(..))
+
+import ParitySolver
+
+data Level = Level { getType :: String
+                     , getID :: Int
+                     , getMode :: String
+                     , getBoard :: [Int]
+                     , getInitialPos :: Position
+                     , getSolution :: Maybe [Direction]
+                     } deriving (Show)
+
+instance FromJSON Level where
+    parseJSON (Object v) = Level
+                           <$> v .: "type"
+                           <*> v .: "number"
+                           <*> v .: "mode"
+                           <*> v .: "contents"
+                           <*> liftM parseInitPos (v .: "initialSelected")
+                           <*> liftM parseDirection (v .:? "solution")
+
+parseInitPos (Object ref) = (getField "x", getField "y")
+  where
+    getField field = case fromJust $ HM.lookup field ref of
+        (Number x) -> fromIntegral $ coefficient x
+        _ -> 0
+
+parseDirection :: Maybe Value -> Maybe [Direction]
+parseDirection Nothing = Nothing
+parseDirection (Just (Array xs)) = Just $ map read dirs
+  where
+    dirs = map (\(String x) -> T.unpack (T.toUpper x)) $ V.toList xs
